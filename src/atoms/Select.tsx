b6b3ba08474d610aa5077af4aa290eece058950e
jamesodwyer <james.odwyer@ticketmaster.co.uk>
2025-09-04 14:34:47 +0100

    style: Update Select component padding to 4px

--- src/atoms/Select.tsx
+++ src/atoms/Select.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Select = styled.select`
-  padding: var(--core-space-2);
+  padding: 4px;
   font: var(--semantic-typography-label-medium);
   min-width: 150px;
 `;
